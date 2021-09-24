7acacac12f2f506386f4c8ed4d546929a16a2430
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-09-24 09:28:10 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+with open("namn.txt","r") as namnFilen:
+    for line in namnFilen:
+        line = line.replace("\n","")
+        print(line)
+
+
+#Printa alla namn som finns i filen
+import os
+import glob
+
+
+txtfiles = []
+for file in glob.glob("*.txt"):
+    txtfiles.append(file)
+
+
+
+print("Meny\n1. Hej\n2.Avsluta")
+
+#ta bort filen om den finns
+if os.path.exists("logg.txt"):
+    os.remove("logg.txt")
+
+
+
+with open("logg.txt", "w") as filen:
+    filen.write("Nu startar programmet\n")
+    filen.write("Hej hej\n")
+
+
+    #allNames = namnFilen.readlines()
+
+#for x in allNames:
+    #print(x)
+
+
+
+
+
+
+
+
+
+
+print("*HUVUDMENY*")
